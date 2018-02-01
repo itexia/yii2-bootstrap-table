c4346b0afc4785ddc6d6f7b29788b28d90feaf5d
Susann Sgorzaly <sgorzaly@portrino.com>
2018-02-01 16:09:33 +0100

    deleted unnecessary dependencies

--- bundles/BootstrapTableExportAssetBundle.php
+++ bundles/BootstrapTableExportAssetBundle.php
@@ -19,17 +19,9 @@ class BootstrapTableExportAssetBundle extends AssetBundle
     public $sourcePath = '@vendor/kayalshri/tableExport.jquery.plugin';
 
     /**
-     * @var array css assets
-     */
-    public $css = [
-        //'bootstrap-table/jquery.dragtable.css',
-    ];
-
-    /**
      * @var array js assets
      */
     public $js = [
-        //'bootstrap-table/jquery.dragtable.js',
       'tableExport.js',
       'jquery.base64.js',
       'html2canvas.js',

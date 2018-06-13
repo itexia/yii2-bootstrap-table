978eb74646fc6034fed855d3d743f7cd8c90bd96
Thomas Grießbach <griessi@gmail.com>
2018-06-13 18:02:43 +0200

    [TASK] adds confirm before sending delete queries, refs #223

--- assets/handleAjaxRequest.js
+++ assets/handleAjaxRequest.js
@@ -14,19 +14,32 @@ function handleAjaxLink(e) {
         url: targetUrl,
         type: "post",
         dataType: 'json',
+        beforeSend: function(xhr, opts){
+            if ($link.attr('id') === 'delete') {
+                var xconfirm = confirm(getTranslation("InvInterface", "Möchten Sie diese Datensätze wirklich löschen?"));
+                if (xconfirm !== true) {
+                    xhr.abort();
+                    return false;
+                }
+            }
+        },
         data: {
             allSelected: $tableOptions.summaryData.selectedAll,
             ids: JSON.stringify($tableOptions.summaryData.rowIds)
         },
         sucess: function (res) {
-            console.log('success');
+            console.log('ajax success');
             console.log(res);
+
+            // TODO: trigger table reload
         },
         error: function (xhr, status, error) {
+            console.log('ajax error');
             console.log(xhr);
             console.log(xhr.responseText);
         },
         complete: function (xhr) {
+            console.log('ajax complete');
             console.log(xhr);
         }
     });
@@ -34,4 +47,27 @@ function handleAjaxLink(e) {
     console.log(targetUrl);
     console.log($tableOptions.summaryData.selectedAll);
     console.log($tableOptions.summaryData.rowIds);
+}
+
+
+function getTranslation(category, message, params) {
+    // TODO: does not exist yet?
+    ajaxRequest = $.ajax({
+        url: 'get-translation',
+        type: "post",
+        async: false,
+        dataType: 'json',
+        data: {
+            category: category,
+            message: message,
+            params: JSON.stringify(params)
+        },
+        success: function (response) {
+            message = response;
+        },
+        error: function (e) {
+        }
+    });
+
+    return message;
 }
